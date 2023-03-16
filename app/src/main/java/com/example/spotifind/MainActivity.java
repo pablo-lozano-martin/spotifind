6d45dd7d841c55cca011bc7a17f2c6abad2cfc84
manuespi <98966768+manuespi@users.noreply.github.com>
2023-03-16 13:31:42 +0100

    Splash screen 1 try

--- app/src/main/java/com/example/spotifind/MainActivity.java
+++ app/src/main/java/com/example/spotifind/MainActivity.java
@@ -3,13 +3,31 @@ package com.example.spotifind;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        showSplashScreen();
+    }
+
+
+    private void showSplashScreen() {
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, RadarActivity.class);
+                //startActivity(intent);
+                finish();
+            }
+        }, 5000); // muestra la splash screen por 5 segundos antes de cargar la actividad principal
     }
 }
---
+++ app/src/main/java/com/example/spotifind/RadarActivity.java
@@ -0,0 +1,10 @@
+package com.example.spotifind;
+
+import android.os.Bundle;
+
+public class RadarActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+}

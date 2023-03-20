f97772f378f88c55fa1f12322ed1a80b18c0da57
Pablo Lozano <paloza01@ucm.es>
2023-03-20 18:27:49 +0100

    cambio de Fragments con la navigation bar
    Todavía no funciona 100%

--- app/src/main/java/com/example/spotifind/MainActivity.java
+++ app/src/main/java/com/example/spotifind/MainActivity.java
@@ -2,14 +2,48 @@ package com.example.spotifind;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.spotifind.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
+        replaceFragment(new RadarFragment()); //Comienza con el fragment del radar al iniciar la app
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.friendlist:
+                    replaceFragment(new FriendlistFragment());
+                    break;
+                case R.id.radar:
+                    replaceFragment(new RadarFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){ //Intercambia el fragment actual por otro
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }

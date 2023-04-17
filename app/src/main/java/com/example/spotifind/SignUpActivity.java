172f8d82ccc3db8cda272948c5e5f6a75b652409
Morwar22 <78794067+Morwar22@users.noreply.github.com>
2023-04-17 18:54:20 +0200

    Primeros Pasos con Firebase

---
+++ app/src/main/java/com/example/spotifind/SignUpActivity.java
@@ -0,0 +1,113 @@
+package com.example.spotifind;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignUpActivity";
+
+    // Firebase Authentication
+    private FirebaseAuth mAuth;
+
+    // Views
+    private EditText mNameField;
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private EditText mPasswordConfirmField;
+    private Button mSignUpButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+
+        // Inicializar Firebase Authentication
+        mAuth = FirebaseAuth.getInstance();
+
+        // Configurar la interfaz de usuario
+        setInterface();
+    }
+
+    private void setInterface() {
+        // Obtener las referencias a los campos de texto y el botón en el layout
+        mNameField = findViewById(R.id.etName);
+        mEmailField = findViewById(R.id.etEmail);
+        mPasswordField = findViewById(R.id.etPass);
+        mPasswordConfirmField = findViewById(R.id.etPass2);
+        mSignUpButton = findViewById(R.id.btnSignup);
+
+        // Establecer el listener para el botón de registro
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        });
+    }
+
+    private void signUp() {
+        String name = mNameField.getText().toString();
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+        String passwordconfirm = mPasswordConfirmField.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(name)) {
+            Toast.makeText(this, "Please enter name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(passwordconfirm))
+        {
+            Toast.makeText(this, "Please enter password confirm", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.equals(password,passwordconfirm))
+        {
+            Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Crear una cuenta con correo electrónico y contraseña en Firebase Authentication
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Registro exitoso
+                            Log.d(TAG, "createUserWithEmail:success");
+                            Toast.makeText(SignUpActivity.this, "Registration successful!", Toast.LENGTH_SHORT).show();
+                            finish(); // volver a la actividad de inicio de sesión
+                        } else {
+                            // Si el registro falla, mostrar un mensaje al usuario
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUpActivity.this, "Registration failed.", Toast.LENGTH_SHORT).show();
+                            //recreate();?
+                        }
+                    }
+                });
+    }
+}

043e58bccc8e64eaac3a3c66699ef82076e6b6fe
manuespi <manuespi@ucm.es>
2023-04-21 13:37:12 +0200

    navBar

--- app/src/main/java/com/example/spotifind/FriendlistActivity.java
+++ app/src/main/java/com/example/spotifind/FriendlistActivity.java
@@ -1,19 +1,29 @@
 package com.example.spotifind;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.File;
 import java.util.ArrayList;
 
 public class FriendlistActivity extends AppCompatActivity implements FriendlistAdapter.OnItemClickListener {
 
     private RecyclerView friendlistRecyclerView;
+    private BottomNavigationView navBar;
     private FriendlistAdapter adapter;
-    private ArrayList<LocalUser> friendlist;
+    private ArrayList<User> friendlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +36,21 @@ public class FriendlistActivity extends AppCompatActivity implements FriendlistA
 
     private void setfriendlist(){
         //TODO set the friendlist
-        this.friendlist = new ArrayList<LocalUser>();
+        this.friendlist = new ArrayList<User>();
     }
 
-    private void setInterface(){
+    private void setInterface() {
         //Recycler view config
         this.friendlistRecyclerView = findViewById(R.id.friendlist_recyclerview);
         this.adapter = new FriendlistAdapter(this, this.friendlist);
         this.adapter.setOnItemClickListener(this);
         this.friendlistRecyclerView.setAdapter(this.adapter);
         this.friendlistRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        navBar = findViewById(R.id.navbar);
+        navBar.setSelectedItemId(R.id.friendlist);
+        NavigationBarListener navigationBarListener = new NavigationBarListener(this);
+        navBar.setOnNavigationItemSelectedListener(navigationBarListener);
     }
 
     @Override

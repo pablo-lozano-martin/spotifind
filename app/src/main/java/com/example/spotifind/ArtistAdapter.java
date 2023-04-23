b40796a1f494828b4f7d8f47e46121ebb2e22499
Pablo Lozano <paloza01@ucm.es>
2023-04-23 13:09:11 +0200

    Perfil artistas falta linkearlo con la api de spotify

--- app/src/main/java/com/example/spotifind/ArtistAdapter.java
+++ app/src/main/java/com/example/spotifind/ArtistAdapter.java
@@ -1,5 +1,7 @@
 package com.example.spotifind;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,19 +11,30 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class ArtistAdapter extends RecyclerView.Adapter<ArtistViewHolder> {
-    private List<Artist> artistList;
+public class ArtistAdapter extends RecyclerView.Adapter<ArtistAdapter.ArtistViewHolder> {
 
-    public ArtistAdapter(List<Artist> artistList) {
+    private List<Artist> artistList;
+    private OnItemClickListener onItemClickListener;
+
+    public ArtistAdapter(List<Artist> artistList, OnItemClickListener onItemClickListener) {
         this.artistList = artistList;
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    public void setArtistList(List<Artist> artistList) {
+        this.artistList = artistList;
+        notifyDataSetChanged();
     }
 
     @NonNull
     @Override
     public ArtistViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.artist_card, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_artist, parent, false);
         return new ArtistViewHolder(view);
     }
 
@@ -36,22 +49,35 @@ public class ArtistAdapter extends RecyclerView.Adapter<ArtistViewHolder> {
         return artistList.size();
     }
 
-    public void setArtistList(List<Artist> artistList) {
-    }
-}
+    class ArtistViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-public class ArtistViewHolder extends RecyclerView.ViewHolder {
-    private ImageView artistImage;
-    private TextView artistName;
+        private ImageView artistImage;
+        private TextView artistName;
 
-    public ArtistViewHolder(View itemView) {
-        super(itemView);
-        artistImage = itemView.findViewById(R.id.artistImage);
-        artistName = itemView.findViewById(R.id.artistName);
+        public ArtistViewHolder(@NonNull View itemView) {
+            super(itemView);
+            artistImage = itemView.findViewById(R.id.artistImage);
+            artistName = itemView.findViewById(R.id.artistName);
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(Artist artist) {
+            artistName.setText(artist.getName());
+            String imageUrl = artist.getImageUrl();
+            if (imageUrl != null && !imageUrl.isEmpty()) {
+                Picasso.get().load(imageUrl).into(artistImage);
+            } else {
+                artistImage.setImageResource(R.drawable.default_image);
+            }
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            if (position != RecyclerView.NO_POSITION && onItemClickListener != null) {
+                onItemClickListener.onItemClick(position);
+            }
+        }
     }
 
-    public void bind(Artist artist) {
-        artistImage.setImageResource(artist.getImageResId());
-        artistName.setText(artist.getName());
-    }
 }
--- app/src/main/java/com/example/spotifind/ProfileActivity.java
+++ app/src/main/java/com/example/spotifind/ProfileActivity.java
@@ -1,5 +1,7 @@
 package com.example.spotifind;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,25 +10,24 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.spotifind.OnItemClickListener;
+import com.example.spotifind.ArtistAdapter.OnItemClickListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements OnItemClickListener {
 
     private TextView textNickname;
-    private TextView artistName;
     private ImageView userImage;
-    private ImageView artistImage;
     private ImageButton editButton;
     private Button spotifyButton;
 
     private RecyclerView recyclerViewTop5Artists;
     private ArtistAdapter artistAdapter;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,32 +35,30 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.profile);
 
         textNickname = findViewById(R.id.textNickname);
-        //artistName = findViewById(R.id.artistName);
         userImage = findViewById(R.id.userImage);
-        //artistImage = findViewById(R.id.artistImage);
         editButton = findViewById(R.id.buttonEdit);
         spotifyButton = findViewById(R.id.buttonSpotify);
         recyclerViewTop5Artists = findViewById(R.id.recyclerViewTop5Artists);
 
+        // Cargar los artistas desde la API de Spotify y agregarlos a la lista
+        List<Artist> artistList = new ArrayList<>();
+        // código para obtener los artistas de la API y agregarlos a la lista
 
-        textNickname.setText("Nombre usuario"); // COGER INFO DEL USUARIO DE LA CLASE USER
-        artistName.setText("Nombre artista"); // INFO DE FIREBASE??
-        //userImage.setImageResource(R.drawable.imagen_usurio);
-        //artistImage.setImageResource(R.drawable.imagen_artista);
-
-
-        artistAdapter = new ArtistAdapter(); // crea una instancia de ArtistAdapter
+        artistAdapter = new ArtistAdapter(artistList, new ArtistAdapter.OnItemClickListener() {
+            @Override
+            public void onArtistClick(Artist artist) {
+                // Abrir la página del artista en la aplicación de Spotify
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(artist.getExternalUrls().getSpotify()));
+                startActivity(intent);
+            }
+        });
 
         // Configurar RecyclerView
         recyclerViewTop5Artists.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewTop5Artists.setAdapter(artistAdapter);
 
-        // Agregar datos al adapter
-        List<Artist> artistList = new ArrayList<>();
-        // Agregar artistas a la lista
-        artistAdapter.setArtistList(artistList);
-
-        //listener botón editar el perfil
+        // Listener botón editar el perfil
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -67,12 +66,21 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-        // listener botón "Spotify"
+        // Listener botón Spotify
         spotifyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // REDIRIGIR A SPOTI
+                // REDIRIGIR A SPOTIFY
             }
         });
     }
+
+    @Override
+    public void onItemClick(int position) {
+        Artist artist = artistList.get(position);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(artist.getExternalUrls().getSpotify()));
+        startActivity(intent);
+    }
 }
+
